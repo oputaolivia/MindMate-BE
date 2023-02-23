5f54edbbcded88cd529f1022e11f89062eb4efd5
Oputa Olivia Amarachi <72948572+oputaolivia@users.noreply.github.com>
2023-02-23 19:11:29 +0000

    changed database uri

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ const app = express();
 dotenv.config();
 connectDB();
 
-app.use(cors());
+const corsOptions = {
+    origin: "*",
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+  };
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
